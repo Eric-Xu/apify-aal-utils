0b36a7b8d760fcd9ff2189473ca13d02e36d8a70
Eric Xu <exu.data@gmail.com>
2024-09-01 22:58:28 -0400

    Add is_unconfirmed() to GoogleMapsUspsAddress.

--- apify_aal_utils/geography.py
+++ apify_aal_utils/geography.py
@@ -4,8 +4,10 @@ from google.maps.addressvalidation_v1.types.address_validation_service import (
     ValidationResult,
 )
 
-
 ADDRESS_CONFIRMATION_THRESHOLD = 2
+STREET_NUMBER_KEY = "street_number"
+ROUTE_KEY = "route"
+
 
 @dataclass
 class GoogleMapsUspsAddress:
@@ -14,7 +16,9 @@ class GoogleMapsUspsAddress:
 
     https://developers.google.com/maps/documentation/address-validation/reference/rest/v1/TopLevel/validateAddress#uspsaddress
     """
+
     validation_result: ValidationResult
+
     def __post_init__(self):
         self.first_address_line: str = (
             self.validation_result.usps_data.standardized_address.first_address_line.title()
@@ -40,6 +44,10 @@ class GoogleMapsUspsAddress:
         self.longitude: float = self.validation_result.geocode.location.longitude
         self.long_address: str = self._get_long_address()
         self.short_address: str = self._get_short_address()
+        self.unconfirmed_components = (
+            self.validation_result.address.unconfirmed_component_types
+        )
+
     def _get_long_address(self) -> str:
         address_lines = ", ".join(
             [l for l in [self.first_address_line, self.second_address_line] if l]
@@ -49,22 +57,31 @@ class GoogleMapsUspsAddress:
         )
         address = f"{address_lines}, {self.city}, {self.state_code} {zip_code_w_extension}, {self.country_code}"
         return address
+
     def _get_short_address(self) -> str:
         address_lines = ", ".join(
             [l for l in [self.first_address_line, self.second_address_line] if l]
         )
         address = f"{address_lines}, {self.city}, {self.state_code} {self.zip_code}"
         return address
-    def is_suspicious(self) -> bool:
-        suspicious_components = dict()
+
+    def _raise_if_missing(self, component_key: str) -> None:
+        exists = False
         for component in self.validation_result.address.address_components:
-            component_text: str = component.component_name.text
-            confirmation_level_numeric: int = component.confirmation_level.value
-            confirmation_level_text = str(component.confirmation_level)
-            if confirmation_level_numeric > ADDRESS_CONFIRMATION_THRESHOLD:
-                print(component)
-                suspicious_components[confirmation_level_text] = component_text
-        is_suspicious = bool(suspicious_components)
-        if is_suspicious:
-            print(f"Suspicious Address Components: {suspicious_components}")
-        return is_suspicious
+            if component.component_type == component_key:
+                exists = True
+        if not exists:
+            raise ValueError(f"Missing Address Component Key: '{component_key}'")
+
+    def is_unconfirmed(self) -> bool:
+        self._raise_if_missing(ROUTE_KEY)
+        self._raise_if_missing(STREET_NUMBER_KEY)
+        is_unconfirmed = (
+            STREET_NUMBER_KEY in self.unconfirmed_components
+            or ROUTE_KEY in self.unconfirmed_components
+        )
+        if is_unconfirmed:
+            print(
+                f"The address '{self.short_address}' has unconfirmed components {self.unconfirmed_components}"
+            )
+        return is_unconfirmed
